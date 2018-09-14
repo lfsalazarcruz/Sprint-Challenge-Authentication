d0bc04bc73572235afa5b9ba7343a3ea7289fb94
Luis Fernando Salazar <lfsalazarcruz@gmail.com>
2018-09-14 08:44:51 -0700

    Implemented generateToken function

--- config/middlewares.js
+++ config/middlewares.js
@@ -5,6 +5,7 @@ const jwtKey = require('../_secrets/keys').jwtKey;
 // quickly see what this file exports
 module.exports = {
   authenticate,
+  generateToken
 };
 
 // implementation details
@@ -24,4 +25,15 @@ function authenticate(req, res, next) {
       error: 'No token provided, must be set on the Authorization Header',
     });
   }
+}
+
+// generate token for authentication
+function generateToken(user) {
+  const payload = {
+    username: user.username,
+  };
+  const options = {
+    expiresIn: '1h',
+  };
+  return jwt.sign(payload, jwtKey, options);
 }
